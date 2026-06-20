b2b0ffbd44a1e92a9330dee08b459e2e1ffbcb8b
PI U <163493878+Hyungpiu@users.noreply.github.com>
2026-06-21 01:46:46 +0700

    add google analytics

--- app/layout.tsx
+++ app/layout.tsx
@@ -1,4 +1,5 @@
 import type { Metadata } from "next";
+import Script from "next/script";
 import { Geist, Geist_Mono } from "next/font/google";
 import "./globals.css";
 
@@ -32,8 +33,23 @@ export default function RootLayout({
       className={`${geistSans.variable} ${geistMono.variable} h-full antialiased`}
     >
       <body className="min-h-full flex flex-col">
-        {children}
-      </body>
+  {children}
+
+  <Script
+    src="https://www.googletagmanager.com/gtag/js?id=G-7FWNZZ1S36"
+    strategy="afterInteractive"
+  />
+
+  <Script id="google-analytics" strategy="afterInteractive">
+    {`
+      window.dataLayer = window.dataLayer || [];
+      function gtag(){dataLayer.push(arguments);}
+      gtag('js', new Date());
+
+      gtag('config', 'G-7FWNZZ1S36');
+    `}
+  </Script>
+</body>
     </html>
   );
 }
